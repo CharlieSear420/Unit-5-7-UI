f93ec4c1f7d3dfc1fa1eaea4c4767ec7118f9d19
CharlieSear420 <72386551+CharlieSear420@users.noreply.github.com>
2021-03-25 16:16:01 +0000

    all main menu buttons working

---
+++ Assets/Scripts/Menu_Gen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Gen : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+        
+    }
+
+    public void StartGame()
+    {
+        
+        SceneManager.LoadScene("Dummy Level");
+    }
+
+    public void Return()
+    {
+        
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
